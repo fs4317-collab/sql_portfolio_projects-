83f3404650228d6c114184729b5e6163a7721b89
Felix Soto Toro <fs4317@gmail.com>
2026-04-12 00:16:20 -0500

    Create create_database.sql

---
+++ project_1_database_setup/create_database.sql
@@ -0,0 +1,11 @@
+-- ============================================
+-- Project: Business Analytics Database
+-- File: create_database.sql
+-- ============================================
+
+-- Description:
+-- This script creates the main database used for analysis.
+
+CREATE DATABASE business_analytics_db;
+
+USE business_analytics_db;
